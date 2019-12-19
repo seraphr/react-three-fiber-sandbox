6914f5ea8f33c083a4623672de6790d55fa008ef
seraph <serph.sub@gmail.com>
2019-12-20 02:22:40 +0900

    three / react-three-fiber の追加とサンプルコード

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import logo from './logo.svg';
+import { Canvas, useFrame } from 'react-three-fiber'
 import './App.css';
+import { Mesh } from 'three';
+
+function Thing() {
+  const ref = useRef<Mesh>(null)
+  useFrame(() => {
+    if (ref !== null && ref.current !== null){
+      ref.current.rotation.x = ref.current.rotation.y += 0.01
+    }
+  })
+  return (
+    <mesh
+      ref={ref}
+      onClick={e => console.log('click')}
+      onPointerOver={e => console.log('hover')}
+      onPointerOut={e => console.log('unhover')}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshNormalMaterial attach="material" />
+    </mesh>
+  )
+}
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <Thing />
+      </Canvas>
     </div>
   );
 }
